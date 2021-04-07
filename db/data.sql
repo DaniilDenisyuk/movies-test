edbe48e98babf64183f3d16cab1f16d4a8b4d587
Daniil Denisyuk <ordekonec@gmail.com>
2021-04-07 13:05:52 +0300

    changed db structure

--- db/data.sql
+++ db/data.sql
@@ -1,9 +1,38 @@
-INSERT INTO Movie (Title, Release_year, Format, Stars) VALUES
-  ('test1',2000, 'DVD', 'star1, star2, star3'),
-  ('test2',2001, 'DVD', 'star1, star2, star3'),
-  ('test3',2002, 'DVD', 'star1, star2, star3'),
-  ('test4',2003, 'Blu-ray', 'star1, star2, star3'),
-  ('test5',2011, 'DVD', 'star1, star2, star3'),
-  ('test6',2005, 'DVD', 'star1, star2, star3'),
-  ('test7',1997, 'DVD', 'star1, star2, star3');
- 
+INSERT INTO Movie (Title, ReleaseYear, Format) VALUES
+  ('test1',2000, 'format1'),
+  ('test2',2001, 'format2'),
+  ('test3',2002, 'format3'),
+  ('test4',2003, 'format4'),
+  ('test5',2011, 'format5'),
+  ('test6',2005, 'format6'),
+  ('test7',1997, 'format7');
+
+  INSERT INTO Actor (FirstName, LastName) VALUES
+  ('firstname1', 'lastname1'),
+  ('firstname2', 'lastname2'),
+  ('firstname3', 'lastname3'),
+  ('firstname4', 'lastname4'),
+  ('firstname5', 'lastname5'),
+  ('firstname6', 'lastname6');
+
+  INSERT INTO ActorMovie (ActorId, MovieId) VALUES
+  (1, 1),
+  (1, 2),
+  (1, 4),
+  (2, 5),
+  (2, 6),
+  (2, 7),
+  (3, 1),
+  (3, 2),
+  (3, 3),
+  (4, 4),
+  (4, 5),
+  (4, 6),
+  (5, 7),
+  (5, 1),
+  (5, 2),
+  (6, 3),
+  (6, 4),
+  (6, 5);
+
+
--- db/structure.sql
+++ db/structure.sql
@@ -1,10 +1,25 @@
 CREATE TABLE Movie (
   Id   serial,
   Title varchar(255) NOT NULL,
-  Release_year varchar(255) NOT NULL,
-  Format varchar(255) NOT NULL,
-  Stars varchar(255) NOT NULL
+  ReleaseYear varchar(255) NOT NULL,
+  Format varchar(255) NOT NULL
 );
+ALTER TABLE Movie ADD CONSTRAINT pkMovie PRIMARY KEY (Id);
+CREATE UNIQUE INDEX akMovie ON Movie (Title, ReleaseYear);
 
-ALTER TABLE Movie ADD CONSTRAINT pkRubric PRIMARY KEY (Id);
-CREATE UNIQUE INDEX akMovie ON Movie (Title, Release_year);
+CREATE TABLE Actor (
+  Id   serial,
+  FirstName varchar(255) NOT NULL,
+  LastName varchar(255) NOT NULL
+);
+ALTER TABLE Actor ADD CONSTRAINT pkActor PRIMARY KEY (Id);
+CREATE UNIQUE INDEX akActor ON Actor (FirstName, LastName);
+
+CREATE TABLE ActorMovie (
+  ActorId INTEGER NOT NULL,
+  MovieId INTEGER NULL
+);
+ALTER TABLE ActorMovie ADD CONSTRAINT pkActorMovie PRIMARY KEY (MovieId, ActorId);
+ALTER TABLE ActorMovie ADD CONSTRAINT fkActorMovieMovieId FOREIGN KEY (MovieId) REFERENCES Movie (Id) ON DELETE CASCADE;
+ALTER TABLE ActorMovie ADD CONSTRAINT fkActorMovieActorId FOREIGN KEY (ActorId) REFERENCES Actor (Id) ON DELETE CASCADE;
+
